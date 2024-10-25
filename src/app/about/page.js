1b34870ab33e893d120109c19b2e54beedee1941
Hyun <hwn123h@naver.com>
2024-10-25 22:15:57 +0900

    about pagE

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,13 +1,36 @@
 import './about.style.css'
 
-export default function about() {
+export default async function about() {
 
-    fetch(`${process.env.NEXT_PUBLIC_DB_API_URL}/about`)
-        .then(response => response.json())
-        .then(result =>
-            console.log(result))
+    const response = await fetch(`${process.env.NEXT_PUBLIC_DB_API_URL}/about`)
+    const data = await response.json();
 
     return (
-        <h1>About page</h1>
+        <>
+            <h1>About page</h1>
+            <div className="about-container">
+                #{data[0].title}
+                <div className="subtitle">
+                    # .{data[0].subtitle}
+                </div>
+                <div className="intro">
+                    {data[0].intro.split("\n").map((line) => (
+                        <p style={{ padding: "8px 0" }}>{line}</p>
+                    ))}
+
+                </div>
+
+                <div className="theme">
+                    # .{data[0].theme}
+                </div>
+                <div className="message">
+                    {data[0].message.split(".").map((line) => (
+                        <p>{line}</p>
+                    ))}
+                </div>
+            </div>
+
+        </>
+
     )
 }
